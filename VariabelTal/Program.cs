392159791cc2c6a381480b4286a7ca0f98db352c
RikkeSunke <admin@TA-8215-10>
2019-08-12 15:30:45 +0200

    Add project files.

---
+++ VariabelTal/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariabelTal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a.ToString());
+            a++;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
